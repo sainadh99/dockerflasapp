04ac400d3b1a7452ef5f412cddd07217fa70a414
Venkata Phanendra Sainadh Narne <venkata.n@prudentconsulting.com>
2025-05-27 15:36:01 -0500

    app.py has been updated

--- app/app.py
+++ app/app.py
@@ -24,8 +24,29 @@ def get_users():
     rows = cur.fetchall()
     cur.close()
     conn.close()
-    users = [{'id': row[0], 'name': row[1]} for row in rows]
+    users = [{'id': row[0], 'name': row[1], 'email': row[2], 'age': row[3]} for row in rows]
     return jsonify(users)
+
+@app.route('/api/users', methods=['POST'])
+def add_user():
+    # Instead of getting JSON input, insert the fixed data you gave:
+    name = 'sainadh'
+    email = 'sainadh@gmail.com'
+    age = 24
+
+    conn = get_db_connection()
+    cur = conn.cursor()
+
+    insert_query = "INSERT INTO users (name, email, age) VALUES (%s, %s, %s) RETURNING id;"
+    cur.execute(insert_query, (name, email, age))
+
+    new_id = cur.fetchone()[0]  # Get the id of inserted row
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    return jsonify({'message': 'User added', 'id': new_id}), 201
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
